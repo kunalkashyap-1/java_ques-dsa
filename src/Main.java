d3fbc9a01b5ba9fa9793412d93fbbc072fb7398d
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-09-18 21:13:04 +0530

    intro java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//we use static because we want to run the code without making an object of class demo
         System.out.println("Hello world!");
+        Scanner input=new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello "+name);
     }
 }
