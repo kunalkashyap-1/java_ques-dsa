7310ffc57999f4aff52742119cfacd60351fd5b7
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-09-26 23:11:09 +0530

    binary search

---
+++ src/binary_2d_array.java
@@ -0,0 +1,30 @@
+public class binary_2d_array {
+    public static void main(String[] args) {
+        int[][] matrix={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+        for(int el:search(matrix,38)){
+            System.out.print(el+" ");
+        }
+    }
+
+    static int[] search(int[][] matrix,int target){
+        int row=0;
+        int col= matrix.length-1;
+        while(row< matrix.length && col>=0){
+            if(matrix[row][col]==target){
+                return new int[]{row,col};
+            }
+            else if(matrix[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
