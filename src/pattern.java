14de614c8c590c7255a18c4d9cf4aa9411f32fba
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-10-04 20:44:48 +0530

    oct 4

---
+++ src/pattern.java
@@ -0,0 +1,24 @@
+public class pattern {
+    public static void main(String[] args) {
+        pattern1(7);
+        pattern2(7);
+    }
+
+    static void pattern1(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
