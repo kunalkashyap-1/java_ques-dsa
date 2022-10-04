f6d283c622bb451a93eab5927bb889ad8485ea6e
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-10-04 21:06:15 +0530

    oct 4 more patterns

--- src/pattern.java
+++ src/pattern.java
@@ -1,7 +1,9 @@
 public class pattern {
     public static void main(String[] args) {
-        pattern1(7);
-        pattern2(7);
+//        pattern1(7);
+//        pattern2(7);
+//        pattern3(5);
+        pattern4(5);
     }
 
     static void pattern1(int n){
@@ -16,6 +18,28 @@ public class pattern {
     static void pattern2(int n){
         for (int i = 1; i <= n; i++) {
             for (int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for (int i = 1; i < 2*n; i++) {
+            int totalCol=i>n?2*n-i:i;
+            for (int k = 0; k < n - totalCol; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= totalCol; j++) {
                 System.out.print("* ");
             }
             System.out.println();
