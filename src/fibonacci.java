b6cc828478f5211c2ae950a53dd8d08c07ab1b07
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-10-09 17:42:44 +0530

    oct 9

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -2,8 +2,13 @@ public class fibonacci {
     public static void main(String[] args) {
         long ans=fibo(12);
         System.out.println(ans);
+        System.out.println(fiboFormula(412));
     }
 
+    static long fiboFormula(int n)
+    {
+        return (long)(Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
+    }
     static long fibo(int n){
         if(n<=1){
             return n;
