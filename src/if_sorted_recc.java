96e0575f197c438040686f6e3576f4f57c9fbf3b
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-10-22 22:10:33 +0530

    oct 22

---
+++ src/if_sorted_recc.java
@@ -0,0 +1,13 @@
+public class if_sorted_recc {
+    public static void main(String[] args){
+        int[] arr= {1,2,3,4,5,6,7,8};
+        System.out.println(check(arr,0));
+    }
+
+    static boolean check(int[] arr,int i){
+        if(i==arr.length-1){
+            return true;
+        }
+        return arr[i]<arr[i+1] && check(arr,++i);
+    }
+}
