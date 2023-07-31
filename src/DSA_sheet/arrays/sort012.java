a1871d3f784f81c996c56b1c8674367df8cc1130
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-07-31 23:00:07 +0530

    012 sort

---
+++ src/DSA_sheet/arrays/sort012.java
@@ -0,0 +1,26 @@
+//package DSA_sheet.arrays;
+//
+//public class sort012 {
+//    public static void sort012(int a[], int n)
+//    {
+//        int low =0, mid = 0, high = a.length -1;
+//        int temp;
+//        while(mid<=high){
+//            if(a[mid] == 0){
+//                temp=a[mid];
+//                a[mid]=a[low];
+//                a[low]=temp;
+//                low++;
+//                mid++;
+//            }
+//            else if(a[mid] == 1){
+//                mid++;
+//            }else{
+//                temp=a[high];
+//                a[high]=a[mid];
+//                a[mid]=temp;
+//                high--;
+//            }
+//        }
+//    }
+//}
